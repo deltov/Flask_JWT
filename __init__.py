15d5f14c50593a1412ef358a0aaf50a0b3612333
deltov <154072038+deltov@users.noreply.github.com>
2025-03-11 14:09:54 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -39,4 +39,4 @@ def protected():
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
-  #commentaire
+  #com
