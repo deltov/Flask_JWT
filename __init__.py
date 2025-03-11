34590f4ae0e7b686ccb866ff5165c719d55ddacc
deltov <154072038+deltov@users.noreply.github.com>
2025-03-11 15:06:41 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask #t
 from flask import render_template
-from flask import json
 from flask import jsonify
 from flask import request
 
@@ -8,19 +7,21 @@ from flask_jwt_extended import create_access_token
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 from flask_jwt_extended import JWTManager
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+
+from datetime import timedelta 
+
+app = Flask(__name__)
+
 # Configuration du module JWT
-app.config["JWT_SECRET_KEY"] = "Ma_clé_secrete"  # Ma clée privée
+app.config["JWT_SECRET_KEY"] = "Ma_clé_secrete" 
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)  # Expiration du token après 1h
 jwt = JWTManager(app)
 
-@app.route('/')
+@app.route('/')  # Test
 def hello_world():
     return render_template('accueil.html')
 
-# Création d'une route qui vérifie l'utilisateur et retour un Jeton JWT si ok.
-# La fonction create_access_token() est utilisée pour générer un jeton JWT.
+# Route de connexion qui génère un token JWT
 @app.route("/login", methods=["POST"])
 def login():
     username = request.json.get("username", None)
@@ -31,13 +32,12 @@ def login():
     access_token = create_access_token(identity=username)
     return jsonify(access_token=access_token)
 
-
 # Route protégée par un jeton valide
 @app.route("/protected", methods=["GET"])
 @jwt_required()
 def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
-                                                                                                               
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
